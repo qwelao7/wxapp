e1f11217d48486478063b666d70422f7a5e3e480
qinkaikai <qinkaikai@afguanjia.com>
2018-07-05 15:29:56 +0800

    视频发布页面调整

--- pages/video_publish/video_publish.js
+++ pages/video_publish/video_publish.js
@@ -2,11 +2,20 @@
 const util = require('../../utils/util.js')
 const qcloud = require('../../utils/wafer2-client-sdk/index');
 const config = require('../../config')
+// 引入 QCloud 小程序增强 SDK
+var uploadFile = require('../../utils/aliyun-vod-upload/uploadAliyun');
+// 引入配置
+var env = require('../../utils/aliyun-vod-upload/env.js');
+var Base64 = require('../../utils/aliyun-vod-upload/Base64.js');
 
 Page({
   data: {
     value: '',
+    content: '',
+    src: '',
+    show: true,
     focus: true,
+    video: '',
     array: [
       {
         communityId: 'd18ddf02-484a-11e8-9faf-48d539affdb4',
@@ -40,9 +49,9 @@ Page({
     console.log('picker发送选择改变，携带值为', e.detail.value)
     var index = e.detail.value;
     this.data.defCommunityId = this.data.array[index].communityId;
-    console.log('defCommunityId', this.data.defCommunityId)
     this.setData({
-      index: e.detail.value
+      index: e.detail.value,
+      value: this.data.defCommunityId
     })
   },
   publish: function () {
@@ -52,8 +61,60 @@ Page({
     })
     setTimeout(function () {
 
-      if (_this.data.value) {
-
+      if ((_this.data.value !== '') && (_this.data.content !== '')) {
+        if (_this.data.src === '') {
+          wx.showToast({
+            icon: 'none',
+            title: '请选择视频'
+          })
+        } else {
+          util.post('http://192.168.200.207:3350/aliyun/video')
+            .then(res => {
+              if (res.status === 100){
+                let data = res.data;
+                console.log(data);
+                const UploadAuthObj = JSON.parse(Base64.decode(data.uploadAuth));
+                const UploadAddressObj = JSON.parse(Base64.decode(data.UploadAddress));
+                _this.setData({
+                  video: data.videoId
+                });
+                env.accessKeyId = UploadAuthObj.AccessKeyId;
+                env.accessKeySecret = UploadAuthObj.AccessKeySecret;
+                env.securityToken = UploadAuthObj.SecurityToken;
+                env.aliyunFileKey = UploadAddressObj.FileName;
+                uploadFile(_this.data.src, "", "",
+                  function (res) {
+                    console.log("上传成功")
+                    let url = 'posts?topicType=15&topicContent=' + _this.data.content + '&communityId=' + _this.data.value + '&videoId=' + _this.data.video;
+                    util.post(url)
+                      .then(res => {
+                        if (res.status === 100) {
+                          wx.showToast({
+                            icon: 'success',
+                            title: '发布成功'
+                          })
+                          setTimeout(function () {
+                            wx.navigateBack({
+                              delta: 1
+                            })
+                          }, 1000)
+                        } else {
+                          console.log(res)
+                          wx.showToast({
+                            title: res.msg
+                          })
+                        }
+                      })
+                  })
+                }
+            })
+            .catch(e=>{
+              console.log(e);
+              wx.showToast({
+                title: "上传失败",
+              })
+            })
+        }
       } else {
         wx.showToast({
           icon: 'none',
@@ -67,5 +128,28 @@ Page({
   onLoad: function () {
 
   },
+
+  contentInput: function (e) {
+    let that = this;
+    that.setData({
+      content: e.detail.value
+    })
+  },
+
+  bindGetVideoButtonTap: function (e) {
+    var that = this
+    wx.chooseVideo({
+      sourceType: ['album', 'camera'],
+      maxDuration: 60,
+      camera: 'back',
+      success: function (res) {
+        console.log(res);
+        that.setData({
+          src: res.tempFilePath,
+          show: false
+        });
+      }
+    })
+  }
 
 })
---
+++ utils/aliyun-vod-upload/env.js
@@ -0,0 +1,22 @@
+// var fileHost=""
+// var config = {
+//   //aliyun OSS config
+// uploadImageUrl: `${fileHost}`,
+// AccessKeySecret: '填你自己的AccessKeySecret',
+// OSSAccessKeyId: '填你自己的 OSSAccessKeyId',
+// timeout: 87600 //这个是上传文件时Policy的失效时间
+// };
+// module.exports = config
+
+//test
+var config = {
+  // UploadAuth: '',
+  // UploadAddress: '',
+  timeout: 3600,
+  accessKeyId:'STS.NHjcHz8qyi2oVFxZZ7JUwfaKe',
+  accessKeySecret: "7Dpe2X3XFXiAtWAUEzjeUorKXGG8myegk1cQxF7eVyBo",
+  aliyunServerURL:"https://in-15fdcf51624911e8b31e00163e1a65b6.oss-cn-shanghai.aliyuncs.com",
+  securityToken:'CAIS2AR1q6Ft5B2yfSjIr4vfKPLO1a5Y3vCEVGDJvlpiRtpbiaTghzz2IHhJeXNvBO0etf4+mWBY7PcYlrUqGsEUGBCVM5AgsswGoVj4JpfZv8u84YADi5CjQZg/qutVmJ28Wf7waf+AUA3GCTmd5MsYo9bTcTGlQCZuW//toJV7b9MRcxClZD5dfrl/LRdjr8lo1xGzUPG2KUzSn3b3BkhlsRYe72Rk8vaHxdaAzRDcgVbmqJcSvJ+jC4C8Ys9gG519XtypvopxbbGT8CNZ5z9A9qp9kM49/izc7P6QH35b4RiNL8/Z7tQNXwhiffobHa9YrfHgmNhlvvDSj43t1ytVOeZcX0akQ5u7ku7ZHP+oLt8jaYvjP3PE3rLpMYLu4T48ZXUSODtDYcZDUHhrEk4RUjXdI6Of8UrWSQC7Wsr217otg7Fyyk3s8MaHAkWLX7SB2DwEB4c4aEokVW4RxnezW6UBaRBpbld7Bq6cV5lOdBRZoK+KzQrJTX9Ez2pLmuD6e/LOs7oDVJ37WZtKyuh4Y49d4U8rVEjPQqiykT0hFgpfTK1RzbPmNLKm9baB25/zW+PdDe0dsVgoWFKGpiGWG3RLNn+ztJ9xaFzdopnfwKfCrso+Sld/v4lSXQveKNs19wU4uKbr5krO+rGkWW2x4m0998Pk5YBJ8gwmXI/937DI4WaD5iPKOPlhyJaJYgA1H0TrIU4e6euJm3cKqyoGl2yJIBsF4zyTrz3oIZBBjaHWnigeU/oDyb6JES3c12FhGM+P3bEHYuprZ9tSScy72Qx5DnRiE9s1HecagAGZMTxeSykCo9/z4Sb9cPOHp7HcKi0uW+A5+Mkm0Y2fS7UxpIM3wgU4uI9nb6VtE2VTFPNS/lCxKHHYZPolyD9pVzX4ZCcHH5P1SKe8L3uYpEMFjB10eIOsZ26T3Tkrq1cKISbQq495PQS/SF4wy3hxU5rd6YPkkuaobzQiIxpZaw==',
+  aliyunFileKey: 'customerTrans/8e322e5abfc4ed14618ab61bee5ceaac/da367a0-164637e9d9c-0006-ff4b-829-09e67.mp4'
+};
+module.exports = config
