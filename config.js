2367ab7d96112aae3166082336102047532c96af
YuChenji <yuchenji1988@vip.qq.com>
2018-07-05 21:36:45 +0800

    切换环境,隐藏更多活动按钮

--- config.js
+++ config.js
@@ -53,37 +53,13 @@ uploadURL : 'https://app-img.huilaila.net:7888/xcx/upload',
    */
 
 // 生产环境
-// pickerInfo : [
-//   {
-//     communityId: '9832d325-51d8-11e8-9faf-48d539affdb4',
-//     name: '濮塘·桃里'
-//   },
-//   {
-//     communityId: 'ee8f78d0-51d7-11e8-9faf-48d539affdb4',
-//     name: '协鑫春风江南'
-//   },
-//   {
-//     communityId: '046ba2c8-328c-11e8-9faf-48d539affdb4',
-//     name: '南京桃花源'
-//   },
-//   {
-//     communityId: '585e764f-328c-11e8-9faf-48d539affdb4',
-//     name: '宝华桃李春风'
-//   },
-//   {
-//     communityId: 'a4352cc4-51c9-11e8-9faf-48d539affdb4',
-//     name: '园博村·桃李春风'
-//   }
-// ],
-
-// 测试环境
   pickerInfo: [
     {
-      communityId: 'd18ddf02-484a-11e8-9faf-48d539affdb4',
+      communityId: '9832d325-51d8-11e8-9faf-48d539affdb4',
       name: '濮塘·桃里'
     },
     {
-      communityId: 'bcbb9597-3eba-11e8-9faf-48d539affdb4',
+      communityId: 'ee8f78d0-51d7-11e8-9faf-48d539affdb4',
       name: '协鑫春风江南'
     },
     {
@@ -95,10 +71,34 @@ uploadURL : 'https://app-img.huilaila.net:7888/xcx/upload',
       name: '宝华桃李春风'
     },
     {
-      communityId: '66dfa671-4392-11e8-9faf-48d539affdb4',
+      communityId: 'a4352cc4-51c9-11e8-9faf-48d539affdb4',
       name: '园博村·桃李春风'
     }
   ],
+
+// 测试环境
+//   pickerInfo: [
+//     {
+//       communityId: 'd18ddf02-484a-11e8-9faf-48d539affdb4',
+//       name: '濮塘·桃里'
+//     },
+//     {
+//       communityId: 'bcbb9597-3eba-11e8-9faf-48d539affdb4',
+//       name: '协鑫春风江南'
+//     },
+//     {
+//       communityId: '046ba2c8-328c-11e8-9faf-48d539affdb4',
+//       name: '南京桃花源'
+//     },
+//     {
+//       communityId: '585e764f-328c-11e8-9faf-48d539affdb4',
+//       name: '宝华桃李春风'
+//     },
+//     {
+//       communityId: '66dfa671-4392-11e8-9faf-48d539affdb4',
+//       name: '园博村·桃李春风'
+//     }
+//   ],
 
 
 };
